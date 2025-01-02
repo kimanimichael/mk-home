0f14db7d597d5777fcefb5360f0862973092a84d
MK <muchunumike@gmail.com>
2025-01-02 03:51:01 +0300

    master: Implement mbed led switching

--- drivers/ports/mbed-common/actuators/led.cpp
+++ drivers/ports/mbed-common/actuators/led.cpp
@@ -2,16 +2,24 @@
 #include "mbed.h"
 #include "cstdio"
 
+LED::LED(const int pin) {
+    _pin = pin;
+}
+
 void LED::LED_init() {
     printf("LED init\n");
 }
 
 
 void LED::LED_on() {
-
+    DigitalOut onboard_led(static_cast<PinName>(_pin));
+    onboard_led.write(1);
     printf("LED on\n");
 }
 
 void LED::LED_off() {
+    DigitalInOut onboard_led(static_cast<PinName>(_pin));
+    onboard_led.output();
+    onboard_led.write(0);
     printf("LED off\n");
 }
