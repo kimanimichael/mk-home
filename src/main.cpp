23e8f985feca211beabaa7f5f51e794675851127
mikael <muchunumike@gmail.com>
2025-11-12 01:02:15 +0300

    f(add_ntc_temp_reading): Get and push temperature readings

--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,23 @@ int main()
     MQTTClient::start();
 
     HTTPClient::get_default_http_instance()->_init();
+    char message[128] = {};
 
     while (1) {
+        float temp = BSP::get_ambient_ntc()->_read();
+
+        snprintf(message, sizeof(message), "{\"ev\":\"sensors\",\"temp\":%.2f, \"brightness\":500, \"pressure\":98 }\n", temp);
 
         BSP::BSP_LED_on();
         MQTTClient::send_mqtt_message("/MK_HOME_NAKUJA", "{\"ev\":\"sensors\",\"temp\":25, \"brightness\":500, \"pressure\":98 }\n");
-        HTTPClient::get_default_http_instance()->_post("{\"ev\":\"sensors\",\"temp\":25, \"brightness\":500, \"pressure\":98 }\n");
+        HTTPClient::get_default_http_instance()->_post(message);
 
-        BSP::BSP_delay(10000);
+        BSP::BSP_delay(5000);
 
         BSP::BSP_LED_off();
         MQTTClient::send_mqtt_message("/MK_HOME_NAKUJA", "{\"ev\":\"devices\",\"fridge\":ON, \"microwave\":OFF, \"NAKUJA_MK\":ON }\n");
-        HTTPClient::get_default_http_instance()->_post("{\"ev\":\"sensors\",\"temp\":25, \"brightness\":500, \"pressure\":98 }\n");
+        HTTPClient::get_default_http_instance()->_post(message);
 
-        BSP::BSP_delay(10000);
+        BSP::BSP_delay(5000);
     }
 }
